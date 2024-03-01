ec030274a1ff0d5df273eb922e7f4492f506deb0
DHANYAVARSHINI <117835173+DHANYAVARSHINI@users.noreply.github.com>
2024-03-01 21:23:16 +0530

    Delete Hello_World_With_NewLine.java

--- Hello_World_With_NewLine.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-import java.util.*;
-
-public class Hello_World_With_NewLine {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World\nHello World");
-    }
-}
