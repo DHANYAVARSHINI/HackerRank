beec05a390c06361a97d5c233f8728568269e312
DHANYAVARSHINI <jdhanyavarshini@gmail.com>
2024-03-01 21:19:29 +0530

    add HackerRank master

---
+++ Hello_World_With_NewLine.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+public class Hello_World_With_NewLine {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World\nHello World");
+    }
+}
