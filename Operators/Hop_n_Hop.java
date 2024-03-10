21e9da3f66186204f54416129c8ad5f67bf7e571
DHANYAVARSHINI J <117835173+DHANYAVARSHINI@users.noreply.github.com>
2024-03-10 22:35:13 +0530

    Add files via upload

---
+++ Operators/Hop_n_Hop.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Hop_n_Hop {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int x1 = 3;
+        int y1 = 4;
+        System.out.println(Math.max((x-x1),(y-y1)));
+    }
+}
